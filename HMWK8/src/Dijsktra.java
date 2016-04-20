11e3f955cbe1b1f371f23c7f50436e8eae7adb50
abelamadou <aamadou@msudenver.edu>
2016-04-20 16:04:35 -0600

    Dean super messed up

--- HMWK8/src/Dijsktra.java
+++ HMWK8/src/Dijsktra.java
@@ -3,9 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by abelamadou on 4/19/16.
@@ -31,27 +29,64 @@ public class Dijsktra {
         System.out.println();
         printArray(Arrays.deepToString(linkCostArray).split("]," ));
 
-
         initDijsktra(linkCostArray);
-
 
     }
 
     private void initDijsktra(int[][] linkCostArray) {
         int D[] = new int[linkCostArray.length]; D[0] = 0;
         int P[] = new int[linkCostArray.length]; P[0] = -1;
-//        int N[] = new int[linkCostArray.length]; N[0] = 0;
         ArrayList<Integer> N = new ArrayList<>(); N.add(0,0);
-        Link Y[] = new Link[linkCostArray.length];
+        ArrayList<Link> Y = new ArrayList<>();
 
         for (int i = 1; i < linkCostArray.length; i++){
             if(adjacent(0,i,linkCostArray)){
                 D[i] = linkCostArray[0][i]; P[i] = 0;
             } else { D[i] = -1; P[i] = -1;}
         }
-
         printArray(D, P, N, Y);
+        continueDijsktra(D, P, N, Y, linkCostArray);
+    }
 
+    private void continueDijsktra(int[] d, int[] p, ArrayList<Integer> n, ArrayList<Link> y, int[][] linkCostArray) {
+        int k;
+        while(n.size() != linkCostArray.length){
+            k = minimumD(d,n);
+            n.add(k);
+            Link l = new Link(p[k],k);
+            y.add(l);
+            System.out.println("Node: " + k + " Value: " + d[k] );
+            System.out.println();
+            printArray(d, p, n, y);
+            break;
+        }
+    }
+
+    private int minimumD(int[] d, ArrayList<Integer> n) {
+        int lowestNode = 0;
+        int lowestValue = 0;
+        boolean firstCheck = false;
+
+        for(int i = 0; i < d.length; i++){
+            if(d[i] > 0 && !firstCheck && !notInN(i, n)){
+                lowestNode = i;
+                lowestValue = d[i];
+                firstCheck = true;
+            } else if(d[i] > 0 && firstCheck && d[i] < lowestValue && !notInN(i, n)){
+                lowestNode = i;
+                lowestValue = d[i];
+            }
+        }
+        return lowestNode;
+    }
+
+    private boolean notInN(int i, ArrayList<Integer> n){
+        for(int x = 0; x < n.size(); x++){
+            if(i == n.get(x)){
+                return true;
+            }
+        }
+        return false;
     }
 
     private boolean adjacent(int x, int y, int[][] linkCostArray) {
@@ -92,7 +127,7 @@ public class Dijsktra {
         }
     }
 
-    private void printArray(int [] arrayD, int [] arrayP, ArrayList <Integer> arrayN, Link [] arrayY ) {
+    private void printArray(int [] arrayD, int [] arrayP, ArrayList <Integer> arrayN, ArrayList<Link> arrayY ) {
         System.out.print("\nN' => [ ");
         for ( int i : arrayN){
             System.out.print(i + " ");
